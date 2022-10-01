8b7d9c976c03c97fbeb22e95a9fd978babcfc0cb
zeming666 <zr109@uowmail.edu.au>
2022-10-02 04:33:18 +1100

    gui

--- gui.py
+++ gui.py
@@ -1,8 +1,10 @@
-from tkinter import *
-import pynvml
+from tkinter import Tk, Label, LEFT
+from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetTemperature, nvmlDeviceGetFanSpeed, \
+    nvmlDeviceGetPowerUsage, nvmlDeviceGetUtilizationRates, nvmlDeviceGetClockInfo, nvmlDeviceGetMemoryInfo, \
+    nvmlDeviceGetTotalEnergyConsumption, nvmlShutdown
 
 root = Tk()
-root.geometry('400x200+50+50')
+root.geometry('400x230+50+50')
 root.title("GPU Monitor")
 
 root.attributes("-alpha", 0.8)
@@ -10,37 +12,39 @@ root.attributes("-topmost", True)
 
 
 def closeWindow():
-    pynvml.nvmlShutdown()
+    nvmlShutdown()
     root.destroy()
 
 
 root.protocol('WM_DELETE_WINDOW', closeWindow)
 
-# 设置文本标签
-lb = Label(root, font=("微软雅黑", 20, "bold"), justify=LEFT, bg='white', fg="black")
+lb = Label(root, font=("microsoft yahei", 18, "bold"), justify=LEFT, bg='white', fg="black")
 lb.pack(anchor="w", fill="both", expand=1)
 
 
-#
-
-
-def showtime():
-    pynvml.nvmlInit()
-    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-    gpuTemperature = pynvml.nvmlDeviceGetTemperature(handle, 0)
+def show():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    gpuTemperature = nvmlDeviceGetTemperature(handle, 0)
     temper = "Temperature: " + str(gpuTemperature) + " °C"
-    Fan1 = pynvml.nvmlDeviceGetFanSpeed(handle)
+    Fan1 = nvmlDeviceGetFanSpeed(handle)
     gpuFan = "GPU Fan: " + str(Fan1) + " %"
-    power = "Power usage: " + str(round(pynvml.nvmlDeviceGetPowerUsage(handle) / 1000, 1)) + " W"
-    utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
+    power = "Power usage: " + str(round(nvmlDeviceGetPowerUsage(handle) / 1000, 1)) + " W"
+    utilization = nvmlDeviceGetUtilizationRates(handle)
     occ = "GPU Occ: " + str(utilization.gpu) + " %"  # gpu利用率
-    res = temper + "\n" + gpuFan + "\n" + power + "\n" + occ
+
+    clock = nvmlDeviceGetClockInfo(handle, 0)
+    clockR = "Clock: " + str(clock) + " MHz"
+    memory = nvmlDeviceGetMemoryInfo(handle)
+    memoryUsed = "Memroy: " + str(round(memory.used / memory.total * 100, 1)) + "%"
+    energy = round(nvmlDeviceGetTotalEnergyConsumption(handle) / 3600000000, 4)
+    totEnergy = "Total energy: " + str(energy) + " kWh"
+    res = temper + "\n" + gpuFan + "\n" + occ + "\n" + clockR + "\n" + memoryUsed + "\n" + power + "\n" + totEnergy
     lb.config(text=res)
-    # 每隔 1秒钟执行time函数
-    lb.after(3000, showtime)
+
+    lb.after(3000, show)
 
 
-showtime()
-# 显示窗口
+show()
 
 root.mainloop()

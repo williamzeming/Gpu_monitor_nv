22d271019dcaf9989fc13bbd7d0aafbe8b5952ab
zeming666 <zr109@uowmail.edu.au>
2022-11-15 01:42:45 +1100

    for laptop

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetTemperatur
     nvmlDeviceGetTotalEnergyConsumption, nvmlShutdown
 
 root = Tk()
-root.geometry('400x230+50+50')
+root.geometry('400x240+50+50')
 root.title("GPU Monitor")
 
 root.attributes("-alpha", 0.8)
@@ -23,26 +23,62 @@ lb.pack(anchor="w", fill="both", expand=1)
 
 
 def show():
+    info = []
     nvmlInit()
     handle = nvmlDeviceGetHandleByIndex(0)
-    gpuTemperature = nvmlDeviceGetTemperature(handle, 0)
-    temper = "Temperature: " + str(gpuTemperature) + " °C"
-    Fan1 = nvmlDeviceGetFanSpeed(handle)
-    gpuFan = "GPU Fan: " + str(Fan1) + " %"
-    power = "Power usage: " + str(round(nvmlDeviceGetPowerUsage(handle) / 1000, 1)) + " W"
-    utilization = nvmlDeviceGetUtilizationRates(handle)
-    occ = "GPU Occ: " + str(utilization.gpu) + " %"
 
-    clock = nvmlDeviceGetClockInfo(handle, 0)
-    clockR = "Clock: " + str(clock) + " MHz"
-    memory = nvmlDeviceGetMemoryInfo(handle)
-    memoryUsed = "Memroy: " + str(round(memory.used / memory.total * 100, 1)) + "%"
-    energy = round(nvmlDeviceGetTotalEnergyConsumption(handle) / 3600000000, 4)
-    totEnergy = "Total energy: " + str(energy) + " kWh"
-    res = temper + "\n" + gpuFan + "\n" + occ + "\n" + clockR + "\n" + memoryUsed + "\n" + power + "\n" + totEnergy
+    try:
+        gpuTemperature = nvmlDeviceGetTemperature(handle, 0)
+        temper = "Temperature: " + str(gpuTemperature) + " °C"+ "\n"
+        info.append(temper)
+    except:
+        pass
+
+    try:
+        Fan1 = nvmlDeviceGetFanSpeed(handle)
+        gpuFan = "GPU Fan: " + str(Fan1) + " %"+ "\n"
+        info.append(gpuFan)
+    except:
+        pass
+
+    try:
+        power = "Power usage: " + str(round(nvmlDeviceGetPowerUsage(handle) / 1000, 1)) + " W"+ "\n"
+        info.append(power)
+    except:
+        pass
+
+    try:
+        utilization = nvmlDeviceGetUtilizationRates(handle)
+        occ = "GPU Occ: " + str(utilization.gpu) + " %"+ "\n"
+        info.append(occ)
+    except:
+        pass
+
+    try:
+        clock = nvmlDeviceGetClockInfo(handle, 0)
+        clockR = "Clock: " + str(clock) + " MHz"+ "\n"
+        info.append(clockR)
+    except:
+        pass
+    try:
+        memory = nvmlDeviceGetMemoryInfo(handle)
+        memoryUsed = "Memory: " + str(round(memory.used / memory.total * 100, 1)) + "%"+ "\n"
+        info.append(memoryUsed)
+    except:
+        pass
+    try:
+        energy = round(nvmlDeviceGetTotalEnergyConsumption(handle) / 3600000000, 4)
+        totEnergy = "Total energy: " + str(energy) + " kWh"
+        info.append(totEnergy)
+    except:
+        pass
+
+    res = ""
+    for i in info:
+        res = res + i
     lb.config(text=res)
 
-    lb.after(3000, show)
+    lb.after(2000, show)
 
 
 show()
